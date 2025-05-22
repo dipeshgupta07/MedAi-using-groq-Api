98f8871ab70f140230ea6dafd6352d6501f2685d
Dipesh Kumar Gupta <151122993+dipeshgupta07@users.noreply.github.com>
2025-05-22 14:54:39 +0530

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+
+    window.addEventListener('load', function() {
+        const chatBox = document.getElementById('chatBox');
+        chatBox.scrollTop = chatBox.scrollHeight;
+    });
+
+
+// Example: Call this after adding a new message
